6cec93483f5eaf48a2b8a261b1ff4a4c8a909a5e
Mama <Mama>
2016-11-07 12:37:47 +0200

    Codition Operator

---
+++ chapter1/ConditionOperator/src/main/java/greensnow25/tianglearea/Point.java
@@ -0,0 +1,29 @@
+/**
+ * Class Point для нахождения растояния между точками
+ * @ author grensnow25
+ * @ sinse 05.11.16
+ * @version 1
+ * @ return - return distance between points
+ */
+package treanglearea;
+ 
+public class Point {
+
+
+        public double x;
+        public double y;
+
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double distanceTo(Point point) {
+            return Math.sqrt(Math.pow(point.x - this.x,2)+(Math.pow(point.y-this.y,2)));
+        }
+}
+
+
+
+
---
+++ chapter1/ConditionOperator/src/main/java/greensnow25/tianglearea/Triangle.java
@@ -0,0 +1,47 @@
+/**
+ * Class Triangle для нахожения площади треугольнтка по заданым точкам
+ * @ author grensnow25
+ * @ sinse 05.11.16
+ * @version 1
+ */
+package treanglearea;
+ 
+public class Triangle {
+   
+
+    public Point a ;
+    public Point b ;
+    public Point c ;
+
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double area() {
+        /**
+         @param firstside - side from point a to point b
+         @param secondside - side from point a to point c
+         @param thirdside - side from point b to point c
+         @return - result
+         */
+
+        double firstside = a.distanceTo(b);
+        double secondside = a.distanceTo(c);
+        double thirdside = b.distanceTo(c);
+
+          if (firstside + secondside < thirdside || firstside + thirdside < secondside ||
+                  secondside + thirdside < firstside) {
+            System.out.println("Пстроение треугольника невозможно!");
+             return -1;  }
+      else {
+           double p = (firstside + secondside + thirdside) / 2;
+            double result = Math.sqrt(p * (p - secondside) * (p - firstside) * (p - thirdside));
+              return result;
+        }
+
+
+    }
+}
