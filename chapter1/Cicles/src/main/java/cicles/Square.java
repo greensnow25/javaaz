3faee6abe77b4ef7921e392589e425ef2cb71f25
Mama <Mama>
2016-11-15 01:22:27 +0200

    qadratic function

---
+++ chapter1/Cicles/src/main/java/cicles/Square.java
@@ -0,0 +1,42 @@
+/* Класс Square  для вычисления функции квадратного уравнения;
+
+@author  greensnow25
+@since 13.11.16
+@version 1
+
+ */
+package cicles;
+public class Square {
+
+    public int a;
+    public int b;
+    public int c;
+
+   int x;
+
+    public Square (int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+    }
+
+// method of calculating a function of the quadratic equation
+
+    float calculate (int x){
+
+        float y ;
+        y = a* (float) Math.pow(x,2) + b*x +c;
+        return y;
+
+    }
+//method shows the value of the function in the interval from step
+    void show(int start,int finish,int step){
+        System.out.println("Значение фунции в промежутке от  "+start + "  до  "+finish+"  с шагом  " + step);
+        for (start =start; start<finish; start= start+step) {
+            x = start;
+            this.calculate(x);
+           }
+
+    }
+
+}
