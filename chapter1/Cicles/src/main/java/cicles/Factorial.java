4055d93c2979a43e605ba2698d2d3b354c65df38
Mama <Mama>
2016-11-15 15:14:43 +0200

    calc fact

---
+++ chapter1/Cicles/src/main/java/cicles/Factorial.java
@@ -0,0 +1,31 @@
+package cicles;
+/*
+public class Factorial= calculate factorial
+ * @author greensnow25 ;
+ * @sinse 15.11.16;
+ * @version 1;
+ */
+
+public class Factorial {
+
+    /*
+     * public methoh show calculate integer factorial;
+     * @return value - fac
+    */
+    public int show(int x){
+       int fac=x;
+
+        for ( x=x-1; x>0; x--) {
+
+            fac =fac * x;
+        }
+       return fac;
+    }
+    public static void main(String[] args){
+Factorial fact = new Factorial();
+        int fa =fact.show(9);
+             System.out.println(fa);
+    }
+
+}
+
