d4e19dac1998cd13675fd4a948b34483c2bff4af
Mama <Mama>
2016-11-15 15:16:01 +0200

    FactorialTest

---
+++ chapter1/Cicles/src/test/java/cicles/FactorialTest.java
@@ -0,0 +1,23 @@
+package cicles;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+public class FactorialTest {
+
+    /*
+    Testing return value;
+     */
+
+    @Test
+    public void whenEnterParamInConstrthesShowFactorial(){
+        Factorial fact = new Factorial();
+        int factorial = fact.show(9);
+        assertThat(factorial, is (362880));
+
+    }
+
+
+}
