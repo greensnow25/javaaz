fe499a05818b61603c1a6ac91f5c7193d4b2427b
Mama <Mama>
2016-11-18 22:43:14 +0200

    test

---
+++ chapter1/Cicles/src/test/java/cicles/CounterTest.java
@@ -0,0 +1,21 @@
+package cicles;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+public class CounterTest {
+
+   //Testing method of class;
+
+    @Test
+    public void whenEnterIntervalthenReturnSum() {
+        Counter count = new Counter();
+        int coun = count.add(1,5);
+        assertThat(coun, is (15) );
+
+    }
+
+}
