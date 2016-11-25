2532dd87d8c4b058080eb8fe41b9d678cf31a032
Mama <Mama>
2016-11-26 00:20:40 +0200

    home task

--- chapter1/Cicles/src/main/java/cicles/Paint.java
+++ chapter1/Cicles/src/main/java/cicles/Paint.java
@@ -10,22 +10,24 @@ public class Paint {
         StringBuilder sb = new StringBuilder(); // создаем обьект класса Stringbuilder
 
 
-        for (int i=1; i<=h; i++){                    // цикл для высоты
+        for (int i=1;i<=h; i++){
+            int  wieght = 2*h-1;
+            boolean flag = true;
+            for (int j = wieght;j >0; j--) {
 
-            int wieght= 2*h-1 ;                         // ширина пирамиды
-               for (int j = 1;j <= wieght; j++) {      // ширина
+                if (j>wieght-i){
+                    if(flag=true ){
+                    sb.append("1");
+                    flag = false;
+                    }
+                    if (flag==false){
+                        sb.append("0");
+                    }
 
 
-                    if (h-i<=j&& j<=h+i && j%2!=0 && i%2!=0)       //должно находиться в промежутке j = h+ -i,
-                    sb.append("1");                                 //где h это высота, а i - єто столбец.
-                                                                    //получается например если столбец i = 2 , 
-                   else if (h-i<=j && j<=h+i && j%2==0 && i%2==0)    // а  высота h =5, то j  заполняем в
-                    sb.append("2");                                   // промежутке h +  - 3
-
-                     else sb.append(" ");
-
-
-               }
+                } else
+                    sb.append("0");
+            }
             sb.append("\r\n");
         }
         return sb.toString();
@@ -39,7 +41,7 @@ public class Paint {
 
     public static void main (String [] args){
       Paint paint = new Paint ();
-        String piramid = paint.piramid(3);
+        String piramid = paint.piramid(5);
         System.out.println(piramid);
     }
 }
