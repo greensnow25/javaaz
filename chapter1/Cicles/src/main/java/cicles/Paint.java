b835e465c133cb9b105aab91b3160036735ee62a
Mama <Mama>
2016-11-27 22:41:38 +0200

    update

--- chapter1/Cicles/src/main/java/cicles/Paint.java
+++ chapter1/Cicles/src/main/java/cicles/Paint.java
@@ -12,16 +12,14 @@ public class Paint {
 
         for (int i=1;i<=h; i++){
             int  wieght = 2*h-1;
-            boolean flag = true;                   // приравниваем логическое значение равное истине
-            for (int j = 1;j <=wieght; j++) {
+            boolean flag = true;
+            for (int j = wieght;j >0; j--) { //изменили внутренний цикл,теперь цыкл для длинн
+                                            // начинает считать с конца предпологаемой длинны
+                                            //с каждым счетом j уменьшаем на 1
+                if (j<=i){
+                    sb.append("1");
 
-                if (j<=i){                          // условие для постойки пирамиды
-                    sb.append("1");                 // если условие верно, вставляем "1"
-                //    flag= false;}      теперь для того , что бы выполнилось следующее условие приравниваем значение ложь
-              //  if (flag == false){    условие , при котором, если "1" была вставлена, то сделать промежуток
-                 //   sb.append("2");     между символами, в виде символа "2"
-                //    flag = true;        для того, что бы виполнилось условие , при котором мы
-                }                         //заполняем оставшуюся часть поля "0" приравниваем значение "истине"
+                }
 
 
                  else
@@ -42,7 +40,7 @@ public class Paint {
 
     public static void main (String [] args){
       Paint paint = new Paint ();
-        String piramid = paint.piramid(5);
+        String piramid = paint.piramid(3);
         System.out.println(piramid);
     }
 }
