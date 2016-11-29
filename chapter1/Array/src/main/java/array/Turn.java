9a9aa8cf330a3149eb0faa9bcb184ec0a0a9ca2f
Mama <Mama>
2016-11-29 22:59:18 +0200

    turn array

--- chapter1/Array/src/main/java/array/Turn.java
+++ chapter1/Array/src/main/java/array/Turn.java
@@ -1,7 +1,29 @@
 package array;
-
-/**
- * Created by Mama on 21.11.2016.
+/** Class Turn, turn array;
+ * @author grensnow25;
+ * @since 29.11.16;
+ * @version 1;
  */
+
+
 public class Turn {
+
+//method takes an array and turns it
+    public int []back(int line [] ) {
+
+        int n = line.length/2;
+
+             for (int i =0; i<n;i++) {
+
+                int temp= line [line.length-i-1] ;
+                line[line.length-i-1]= line [i];
+                line [i]= temp;
+
+
+
+
+                }return line ;
+
+    }
+
 }
