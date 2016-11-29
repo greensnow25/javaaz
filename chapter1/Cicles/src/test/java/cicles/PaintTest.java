1c36a7c5a8d9ff7f0ee2b2c60d6fc8b33f006ae2
Mama <Mama>
2016-11-29 22:56:29 +0200

    build piramid

--- chapter1/Cicles/src/test/java/cicles/PaintTest.java
+++ chapter1/Cicles/src/test/java/cicles/PaintTest.java
@@ -1,22 +1,18 @@
 package cicles;
 
 import org.junit.Test;
-
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by Mama on 23.11.2016.
- */
+
 public class PaintTest {
     @Test
-    public void piramid() throws Exception {
+    public void whenEnterHieghtthenReturnPiramidOfSymbols()  {
 
         Paint paint = new Paint ();
         String piramid = paint.piramid(3);
 
-        assertThat(piramid, is ("  1  \r\n 2 2 \r\n1 1 1\r\n"));
+        assertThat(piramid, is ("  ^  \r\n ^ ^ \r\n^   ^\r\n"));
     }
 
 }
