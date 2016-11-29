7fe407c641fcfd0434e4c8663546bfbff13ffb3b
Mama <Mama>
2016-11-29 23:00:32 +0200

    turn array test

--- chapter1/Array/src/test/java/array/TurnTest.java
+++ chapter1/Array/src/test/java/array/TurnTest.java
@@ -1,8 +1,23 @@
+package array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by Mama on 29.11.2016.
- */
+
+
 public class TurnTest {
+    @Test
+    public void whenInputArraythenOutTurnArray()  {
+
+        Turn turn = new Turn();                     //creating objeck turn of type Turn()
+        int [] test={1,2,3,4,5,6,7};                // createing testiting array
+        int [] expected ={7,6,5,4,3,2,1};           // creating expected array
+        int [] line = turn.back(test);              // creating a line array and pass it to the method back ()
+
+        assertThat(line, is (expected));
+
+
+    }
 
 }
