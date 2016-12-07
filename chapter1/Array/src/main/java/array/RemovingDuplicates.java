ed93f2dbb8619f605b9f391243c03f2132f437ef
alexandr <greensnow25@yandex.ru>
2016-12-08 00:12:07 +0200

    removes duplikats

--- chapter1/Array/src/main/java/array/RemovingDuplicates.java
+++ chapter1/Array/src/main/java/array/RemovingDuplicates.java
@@ -1,49 +1,36 @@
 package array;
-
+/*
+public class RemovingDuplicates removes duplicates from array
+@author greensnov 25;
+@sinse 08.12.16
+@version 3.0
+ */
 
 public class RemovingDuplicates {
 
 
+    /*
+public method removd removes duplicates from array;
+@param name-description: string array;
+@return name-description: String orray with out duplicats;
+ */
 
-    private String [] removd (String [] words){
-
-        String [] result = new String[words.length ]; // array with out duplicats
-
-            for (int i=0; i<words.length-1;i++){
-                 result[i]= words [i];
-                 for(int j=i+1;j<words.length-1;j++){
-                     if(words [i].equals(words [j])  ) {
-                         result[i] = null;
+    public String [] removd (String [] words){
+        int count=0;
+           for (int i=0; i<words.length-1;i++){
+                for(int j=i+1;j<=words.length-1;j++){
+                     if(words[i] != null && words[j] !=null && words [i].equals(words [j])  ) {
+                         words[j] = null;
+                        count++;
                      }
                  }
             }
-        String [] sort = new String [result.length];             //sort array
-                int count=0;
-            for(int i = 0; i<result.length-1;i++){
-                for(int j=i+1;j<result.length-1;j++){
-                    if(result[i]== null && result[j]!=null ){
-                        sort[i]=result[j];
-                        result[j]=null;
+          String [] small = new String[words.length - count];
+                    count=0;
+                for(int i=0; i<=words.length-1; i++){
 
-                        break;
-                    }
-
-                }
-            }
-
-            for (int i =0;i<=sort.length-1;i++){                // counter
-                if(sort [i]!=null) {
-
-                    count++;
-                }
-            }
-
-            String [] small = new String[count];                 // array with out null
-
-                for(int i=0; i<=sort.length-1; i++){
-
-                        if(sort[i]!=null){
-                            small[i] = sort[i];
+                        if(words[i]!=null){
+                            small[count++] = words[i];
 
                    }
                 }
@@ -53,19 +40,6 @@ public class RemovingDuplicates {
 
 
 
-    public static void main (String [] args){
 
-        RemovingDuplicates remov = new RemovingDuplicates();
-
-        String [] words = new String[]{"Привет", "Привет", "Мир", "Мир", "Меня", "Меня","Зовут", "Александр", "Мир"};
-
-        String [] removd = remov.removd(words);
-
-
-        for (int i=0; i<=removd.length-1;i++){
-            System.out.print(removd [i] + " ");
-        }
-
-    }
 
 }
