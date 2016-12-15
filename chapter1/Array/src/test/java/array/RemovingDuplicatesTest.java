c56ca0acdffb491f09f6b9a80ab188842a1fe453
alexandr <greensnow25@yandex.ru>
2016-12-16 00:01:20 +0200

    pom

--- chapter1/Array/src/test/java/array/RemovingDuplicatesTest.java
+++ chapter1/Array/src/test/java/array/RemovingDuplicatesTest.java
@@ -2,15 +2,24 @@ package array;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertThat;
+/**
+ * public class RemovingDuplicatesTest.
+ * @author greensow25.
+ * @since 30.11.16.
+ * @version 1.
+ */
  public class RemovingDuplicatesTest {
-    @Test
-    public void whenEnterArraythenOutWithOutduplikats() throws Exception {
+    /**
+     *
+     * method.
+     */
+     @Test
+    public void whenEnterArraythenOutWithOutduplikats() {
         RemovingDuplicates remove = new RemovingDuplicates();
-        String[] test = new String  []{"Привет", "Привет", "Мир", "Мир", "Меня", "Меня","Зовут", "Александр", "Мир"};
-        String [] expected = new String[]{"Привет", "Мир", "Меня", "Зовут", "Александр"};
-        String [] result = remove.removd(test);
+        String[] test = new String[]{"Привет", "Привет", "Мир", "Мир", "Меня", "Меня", "Зовут", "Александр", "Мир"};
+        String[] expected = new String[]{"Привет", "Мир", "Меня", "Зовут", "Александр"};
+        String[] result = remove.removd(test);
         assertThat(result, is(expected));
 
     }
