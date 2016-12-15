9c39f6247bcb4dea32ec9781302c483e314d7f05
alexandr <greensnow25@yandex.ru>
2016-12-16 00:00:31 +0200

    Turn

--- chapter1/Array/src/main/java/array/Turn.java
+++ chapter1/Array/src/main/java/array/Turn.java
@@ -1,29 +1,22 @@
 package array;
-/** Class Turn, turn array;
- * @author grensnow25;
- * @since 29.11.16;
- * @version 1;
+/** Class Turn, turn array.
+ * @author grensnow25.
+ * @since 29.11.16.
+ * @version 1.
  */
-
-
 public class Turn {
-
-//method takes an array and turns it
-    public int []back(int line [] ) {
-
-        int n = line.length/2;
-
-             for (int i =0; i<n;i++) {
-
-                int temp= line [line.length-i-1] ;
-                line[line.length-i-1]= line [i];
-                line [i]= temp;
-
-
-
-
-                }return line ;
-
+    /**
+     * method turn array.
+     * @param line array.
+     * @return turn array.
+     */
+    final int[]back(final int[]line) {
+        int n = line.length / 2;
+             for (int i = 0; i < n; i++) {
+                int temp = line [line.length - i - 1];
+                line[line.length - i - 1] = line[i];
+                line[i] = temp;
+                }
+                return line;
     }
-
 }
