e036e06527f0c835b16f6d4be691c90bc0de6bfa
alexandr <greensnow25@yandex.ru>
2016-12-15 23:59:42 +0200

    RemovingDuplicates

--- chapter1/Array/src/main/java/array/RemovingDuplicates.java
+++ chapter1/Array/src/main/java/array/RemovingDuplicates.java
@@ -1,35 +1,34 @@
 package array;
-/*
-public class RemovingDuplicates removes duplicates from array
-@author greensnov 25;
-@sinse 08.12.16
-@version 3.0
+/**
+*public class RemovingDuplicates removes duplicates from array.
+*@author greensnov 25.
+*@since 08.12.16.
+*@version 3.0.
  */
 
 public class RemovingDuplicates {
+    /**
+     * method removd.
+     * @param words line.
+     * @return array with out duplicats.
+     */
 
-
-    /*
-public method removd removes duplicates from array;
-@param name-description: string array;
-@return name-description: String orray with out duplicats;
- */
-
-    public String [] removd (String [] words){
-        int count=0;
-           for (int i=0; i<words.length-1;i++){
-                for(int j=i+1;j<=words.length-1;j++){
-                     if(words[i] != null && words[j] !=null && words [i].equals(words [j])  ) {
+    final String[] removd(final String[] words) {
+        int count = 0;
+           for (int i = 0; i < words.length - 1; i++) {
+                for (int j = i  + 1; j <= words.length - 1; j++) {
+                     if (words[i] != null && words[j] != null
+                             && words[i].equals(words[j])) {
                          words[j] = null;
-                        count++;
+                         count++;
                      }
                  }
             }
-          String [] small = new String[words.length - count];
-                    count=0;
-                for(int i=0; i<=words.length-1; i++){
+          String[] small = new String[words.length - count];
+                    count = 0;
+                for (int i = 0; i <= words.length - 1; i++) {
 
-                        if(words[i]!=null){
+                        if (words[i] != null) {
                             small[count++] = words[i];
 
                    }
