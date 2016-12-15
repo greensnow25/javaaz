9de0aca47fe2bf32db392076f59c8ab38bd04d76
alexandr <greensnow25@yandex.ru>
2016-12-15 23:59:05 +0200

    buble sort

--- chapter1/Array/src/main/java/array/BubleSort.java
+++ chapter1/Array/src/main/java/array/BubleSort.java
@@ -1,21 +1,26 @@
 package array;
-/*public class BubleSort it sort array the method of buble;
-@author greensow25;
-@since 30.11.16;
-@vwrsion 1;
+/**
+ * public class BubleSort it sort array the method of buble.
+ * @author greensow25.
+ * @since 30.11.16.
+ * @version 1.
  */
 
 public class BubleSort {
+    /**
+     *
+     * @param array int.
+     * @return sort array.
+     */
+    final int[] sort(final int[] array) {
 
-    public int [] sort(int [] array){
+        for (int i = 0; i <= array.length - 1; i++) {
 
-        for (int i =0; i<=array.length-1;i++){        //cicle for firrst element
+            for (int j = i + 1; j <= array.length - 1; j++) {
 
-            for(int j=i+1; j<=array.length-1;j++){   // cicle for second element
+                if (array [i] >= array[j]) {
 
-                if(array [i]>=array[j]){             // sort condition
-
-                    int temp= array[i];
+                    int temp = array[i];
                     array [i] = array [j];
                     array [j] = temp;
                 }
