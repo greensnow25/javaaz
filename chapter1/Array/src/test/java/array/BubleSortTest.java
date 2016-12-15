0aa9348214c586ef56c95ad7bc09c117b46840b0
alexandr <greensnow25@yandex.ru>
2016-12-16 00:01:04 +0200

    BubleSortTest

--- chapter1/Array/src/test/java/array/BubleSortTest.java
+++ chapter1/Array/src/test/java/array/BubleSortTest.java
@@ -2,21 +2,31 @@ package array;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
+/**
+ * public class BubleSort it sort array the method of buble.
+ * @author greensow25.
+ * @since 30.11.16.
+ * @version 1.
+ */
 
 public class BubleSortTest {
+    /**
+     * method.
+     */
     @Test
     public void whenEnterArraythenOutSortArray()  {
 
         BubleSort sort = new BubleSort();
-        int [] testarray= new int[7];
-            for (int i =0; i<=testarray.length-1;i++){
-            testarray[i]=(int) (Math. random()*30);
-
+        final int ar = 7;
+        final int arr = 30;
+        int[]testarray = new int[ar];
+            for (int i = 0; i <= testarray.length - 1; i++) {
+            testarray[i] = (int) (Math.random() * arr);
             }
-        int [] expectedarray= sort.sort(testarray);
-            assertThat(expectedarray, is (testarray));
+        int[]expectedarray = sort.sort(testarray);
+            assertThat(expectedarray, is(testarray));
     }
 
 }
