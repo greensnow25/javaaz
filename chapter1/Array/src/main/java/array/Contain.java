83e5d04233d8e3773d8cc811ed385f70634359f4
alexandr <greensnow25@yandex.ru>
2016-12-15 23:55:27 +0200

    contain

--- chapter1/Array/src/main/java/array/Contain.java
+++ chapter1/Array/src/main/java/array/Contain.java
@@ -5,8 +5,6 @@
 */
 package array;
 
-import java.util.Arrays;
-
 /**
  * public class Contain chec for sub on origin.
  * @author greensnow25.
@@ -21,33 +19,24 @@ public class Contain {
      * @return boolean result.
      */
    final boolean contains(final String origin, final String sub) {
-       char[] words = origin.toCharArray();         //разбиваем оригинальную строку на символы
-        char[] small = sub.toCharArray();           // разбиваем подстроку на символы
-        int smallLenngth = small.length;            // узнаем длинну подстроки
+       char[]words = origin.toCharArray();
+        char[]small = sub.toCharArray();
         boolean result = true;
-       int count = 0;                               // счетчик совпадений
-       for (int i = 0; i <= words.length - 1; i++) { //просматриваем оригинальную строку
-           if (words[i] == small[0]) {                  // если первая буква строки и подстроки равны
-               char[] cop = Arrays.copyOfRange(words, i, i+smallLenngth); // копируем в новый массив с промежутком равным длинне подстроки
-               for (int j = 0; j <= cop.length - 1; j++) { // просматриваем скопированый массив
-                         if (cop[j] == small[j]) {          // если символы равны
-                            count++;                        // увеичиваем счетчик на 1
-                             if (count == smallLenngth-1) {  // если количество совпадений равно количеству символов подстроки
-                                result = false;               // выводим результат , что совпадения найдены
-                                count = 0;                    // обнуляем счетчик
+       int count = 0;
+       for (int i = 0; i <= words.length - 1; i++) {
+           if (words[i] == small[0]) {
+
+               for (int j = 0; j <= small.length - 1; j++) {
+                         if (words[j] == small[j]) {
+                            count++;
+                             if (count == small.length) {
+                                result = false;
+                                count = 0;
                              }
-
-
-
-
                          }
-                      }
+               }
            }
-
        }
-
        return result;
    }
-
-
 }
