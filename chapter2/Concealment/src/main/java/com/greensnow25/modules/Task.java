7a6f1d9398996139bcdea681a27c68e594287992
alexandr <greensnow25@yandex.ru>
2016-12-27 22:25:39 +0200

    Task

---
+++ chapter2/Concealment/src/main/java/com/greensnow25/modules/Task.java
@@ -0,0 +1,15 @@
+package com.greensnow25.modules;
+
+/**
+ * Created by
+ */
+public class Task extends Item {
+    public Task(String name, String desc) {
+        this.name = name;
+        this.discription = desc;
+    }
+
+    public Task(){
+
+    }
+}
