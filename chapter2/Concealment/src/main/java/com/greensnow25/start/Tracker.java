9189cb1ec07945546b57b0e22cdf8ded61fe44a8
alexandr <greensnow25@yandex.ru>
2016-12-27 11:50:42 +0200

    Class tracker

--- chapter2/Concealment/src/main/java/com/greensnow25/start/Tracker.java
+++ chapter2/Concealment/src/main/java/com/greensnow25/start/Tracker.java
@@ -2,21 +2,33 @@ package com.greensnow25.start;
 
 import com.greensnow25.modules.*;
 import java.util.Random;
-
-
 /**
- * Created by on 23.12.2016.
+ * public Class Tracker create repository of items and methods perfom actions above then
+ * @author gretnsnoow25.
+ * @since 27.12.16.
+ * @version 1.0.
  */
 public class Tracker {
     private Item[] items = new Item[10];
     private int position = 0;
     private static final Random RN = new Random();
 
+    /**
+     *  Method add the Item.
+     * @param item- item.
+     * @return item onposition.
+     */
     public Item add (Item item) {
         item.setId(this.generateId());
-        this.items[position++] = item;
+        this.items[position ++] = item;
         return item;
     }
+
+    /**
+     * Methos search item by id.
+     * @param id of item.
+     * @return finded item
+     */
     protected Item findById(String id) {
     Item result = null;
         for (Item item : items) {
@@ -27,9 +39,19 @@ public class Tracker {
         }
         return result;
     }
+
+    /**
+     * Method generate id for item.
+     * @return id.
+     */
     String generateId() {
         return String.valueOf(System.currentTimeMillis() + RN.nextInt());
     }
+
+    /**
+     * Method show all items.
+     * @return array of items.
+     */
     public Item[] getAll() {
         Item[] result = new Item [this.position];
         for (int index = 0; index!= this.position;index++) {
@@ -38,19 +60,37 @@ public class Tracker {
         return result;
     }
 
+    /**
+     * Metod search item and update them.
+     * @param item you want to find.
+     */
     public void update(Item item) {
-        this.add(findById(item.getId()));
+        for (int index = 0; index != items.length; index++) {
+            if (items[index] != null && this.items[index].getId().equals(item.getId()));
+                this.items[index]= item;
+            break;
+        }
     }
 
-    public void delete(Item item, String name) {
-        for (int index = 0; index <= items.length - 1; index++) {
-            if (this.items[index] != null && item.getName().equals(findByName(name))){
-               this.items[index] = null;
+    /**
+     * Method search and delete item.
+     * @param item which need to delete.
+     */
+    public void delete(Item item) {
+        for (int index = 0; index != items.length; index++) {
+            if (items[index] != null && items[index].getId().equals(item.getId())) {
+                items[index] = null;
+                position--;
+           break;
             }
         }
-
-
     }
+
+    /**
+     * Method search item by name.
+     * @param key - name of item.
+     * @return finded item.
+     */
     public Item findByName(String key) {
         Item result = null;
             for (Item item : items) {
@@ -61,4 +101,5 @@ public class Tracker {
             }
         return result;
     }
+
 }
