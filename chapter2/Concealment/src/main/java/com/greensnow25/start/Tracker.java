9af22965a40cee5ec32913ca7741adf61a49c625
alexandr <greensnow25@yandex.ru>
2016-12-28 13:57:37 +0200

    Tracker

--- chapter2/Concealment/src/main/java/com/greensnow25/start/Tracker.java
+++ chapter2/Concealment/src/main/java/com/greensnow25/start/Tracker.java
@@ -1,38 +1,56 @@
 package com.greensnow25.start;
 
-import com.greensnow25.modules.*;
+import com.greensnow25.modules.Item;
+
 import java.util.Random;
+
 /**
- * public Class Tracker create repository of items and methods perfom actions above then
+ * public Class Tracker create repository of items and methods perfom actions above then.
+ *
  * @author gretnsnoow25.
- * @since 27.12.16.
  * @version 1.0.
+ * @since 27.12.16.
  */
 public class Tracker {
-    private Item[] items = new Item[10];
+    /**
+     * sizearray.
+     */
+    private final int sizearray = 10;
+    /**
+     * array of items.
+     */
+    private Item[] items = new Item[sizearray];
+    /**
+     * position.
+     */
     private int position = 0;
+    /**
+     * const.
+     */
     private static final Random RN = new Random();
 
     /**
-     *  Method add the Item.
-     * @param item- item.
+     * Method add the Item.
+     *
+     * @param item item.
      * @return item onposition.
      */
-    public Item add (Item item) {
+    public Item add(Item item) {
         item.setId(this.generateId());
-        this.items[position ++] = item;
+        this.items[position++] = item;
         return item;
     }
 
     /**
      * Methos search item by id.
+     *
      * @param id of item.
      * @return finded item
      */
-    protected Item findById(String id) {
-    Item result = null;
+    public Item findById(String id) {
+        Item result = null;
         for (Item item : items) {
-            if (item != null && item.getId().equals(id)){
+            if (item != null && item.getId().equals(id)) {
                 result = item;
                 break;
             }
@@ -42,19 +60,21 @@ public class Tracker {
 
     /**
      * Method generate id for item.
+     *
      * @return id.
      */
-    String generateId() {
+    public String generateId() {
         return String.valueOf(System.currentTimeMillis() + RN.nextInt());
     }
 
     /**
      * Method show all items.
+     *
      * @return array of items.
      */
     public Item[] getAll() {
-        Item[] result = new Item [this.position];
-        for (int index = 0; index!= this.position;index++) {
+        Item[] result = new Item[this.position];
+        for (int index = 0; index != this.position; index++) {
             result[index] = this.items[index];
         }
         return result;
@@ -62,44 +82,56 @@ public class Tracker {
 
     /**
      * Metod search item and update them.
+     *
      * @param item you want to find.
      */
     public void update(Item item) {
         for (int index = 0; index != this.items.length; index++) {
-            if (this.items[index] != null && this.items[index].getId().equals(item.getId()));
-                this.items[index]= item;
-            break;
+            if (this.items[index] != null && this.items[index].getId().equals(item.getId())) {
+                this.items[index] = item;
+                break;
+            }
         }
     }
 
     /**
      * Method search and delete item.
+     *
      * @param item which need to delete.
      */
     public void delete(Item item) {
         for (int index = 0; index != this.items.length; index++) {
             if (this.items[index] != null && this.items[index].getId().equals(item.getId())) {
                 this.items[index] = null;
-                    position--;
-           break;
+                position--;
+                break;
             }
         }
     }
 
     /**
      * Method search item by name.
+     *
      * @param key - name of item.
      * @return finded item.
      */
     public Item findByName(String key) {
-        Item result = null;
-            for (Item item : this.items) {
-                if (item !=null && item.getName().equals(key)) {
-                    result = item;
-                    break;
-                }
+        Item res = null;
+        for (Item item : this.items) {
+            if (item != null && item.getName().equals(key)) {
+                res = item;
+                break;
             }
-        return result;
+        }
+        return res;
     }
 
+    /**
+     * getPosition.
+     *
+     * @return position.
+     */
+    public int getPosition() {
+        return position;
+    }
 }
