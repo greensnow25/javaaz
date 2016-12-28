1a3f5968fd834b0775e6d9403d3193c76f7d9cba
alexandr <greensnow25@yandex.ru>
2016-12-28 13:57:11 +0200

    Start

--- chapter2/Concealment/src/main/java/com/greensnow25/start/StartUI.java
+++ chapter2/Concealment/src/main/java/com/greensnow25/start/StartUI.java
@@ -1,24 +1,33 @@
 package com.greensnow25.start;
 
-import com.greensnow25.modules.*;
+import com.greensnow25.modules.Item;
+import com.greensnow25.modules.Task;
+
 
 /**
- * Created by
+ * public class StartUI run TRACKER.
+ * @author grensnow25.
+ * @since 28.12.16.
+ * @version 1.0.
  */
 public class StartUI {
-    public static void main (String[] args) {
+    /**
+     * main.
+     * @param args string array.
+     */
+    public static void main(String[] args) {
         Tracker tracker = new Tracker();
 
         Task t2 = new Task("first task", "first de1");
         Task t3 = new Task("wqe", "ewq");
         tracker.add(t3);
-       // tracker.add(t2);
-        //tracker.update(t3);
-        tracker.delete(t3);
+        tracker.add(t2);
+        tracker.update(t3);
+        tracker.findByName("wqe");
 
 
         for (Item item : tracker.getAll()) {
-            System.out.println(item.getName() + " " + item.getDiscription());
+            System.out.format(item.getName(), item.getDiscription());
         }
     }
 }
