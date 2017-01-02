3de1834f9df558ccc0912fded63ccfa13338cc2d
alexandr <greensnow25@yandex.ru>
2017-01-02 18:41:01 +0200

    AsotiationTest arrays

---
+++ chapter1/Array/src/test/java/array/AsTest.java
@@ -0,0 +1,27 @@
+package array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * public class AsTest unites two sorted arrays.
+ * @author greensnow25.
+ * @since 02.01.17.
+ * @version 1.0.
+ */
+public class AsTest {
+    /**
+     * method test returned unites array.
+     */
+    @Test
+    public void whenTwoSortedArraysThenReturnUnitesArray()  {
+        As as = new As();
+        final int[] arrayOne = new int[]{1, 2, 4, 6, 8};
+        final int[] arrayTwo = new int[] {3, 5, 7, 9};
+        final int[] unitArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        final int[] result = as.sort(arrayOne, arrayTwo);
+        assertThat(result, is(unitArray));
+    }
+
+}
