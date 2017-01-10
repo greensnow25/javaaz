8a58279ea40f1648110a33d1cc9dfb6ce9468908
alexandr <greensnow25@yandex.ru>
2017-01-10 23:57:28 +0200

    Association of ordered arrays

--- chapter1/Array/src/test/java/array/AsTest.java
+++ chapter1/Array/src/test/java/array/AsTest.java
@@ -18,8 +18,8 @@ public class AsTest {
     public void whenTwoSortedArraysThenReturnUnitesArray()  {
         As as = new As();
         final int[] arrayOne = new int[]{1, 2, 4, 6, 8};
-        final int[] arrayTwo = new int[] {3, 5, 7, 9};
-        final int[] unitArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        final int[] arrayTwo = new int[] {2, 3, 5, 7, 9};
+        final int[] unitArray = new int[]{1, 2, 2, 3, 4, 5, 6, 7, 8, 9};
         final int[] result = as.sort(arrayOne, arrayTwo);
         assertThat(result, is(unitArray));
     }
