0b2954a9e875e918ca9aad63e13a8fa9887115d5
alexandr <greensnow25@yandex.ru>
2017-01-12 13:44:01 +0200

    update class

---
+++ chapter2/Concealment/src/test/java/com/greensnow25/start/StartUITest.java
@@ -0,0 +1,21 @@
+package com.greensnow25.start;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mama on 11.01.2017.
+ */
+public class StartUITest {
+    @Test
+    public void init() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
