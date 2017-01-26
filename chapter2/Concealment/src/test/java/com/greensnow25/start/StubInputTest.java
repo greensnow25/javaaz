db6c166eef4f9f02d59b8185f17be7506a156fa3
alexandr <greensnow25@yandex.ru>
2017-01-27 00:33:13 +0200

    Update date 27.01.17

--- chapter2/Concealment/src/test/java/com/greensnow25/start/StubInputTest.java
+++ chapter2/Concealment/src/test/java/com/greensnow25/start/StubInputTest.java
@@ -5,14 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by Mama on 27.01.2017.
+ * test class trow exeption.
+ *
+ * @author grensow25
+ * @version 1.
+ * @since 27.01.17.
  */
 public class StubInputTest {
 
     @Test(expected = MenuOutExeption.class)
     public void whenTrowExeptionThenReturnLine() throws MenuOutExeption {
         StubInput stubInput = new StubInput(new String[]{"1"});
-        int[] range = new int[]{0,2,6};
+        int[] range = new int[]{0, 2, 6};
 
         stubInput.ask("1", range);
 
