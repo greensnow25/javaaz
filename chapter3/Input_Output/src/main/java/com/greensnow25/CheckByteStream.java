9ecc54c55e39553b5bc5cf88aad60e3e4f3b90aa
alexandr <greensnow25@yandex.ru>
2017-02-14 00:34:54 +0200

    Update date 14.02.17

---
+++ chapter3/Input_Output/src/main/java/com/greensnow25/CheckByteStream.java
@@ -0,0 +1,33 @@
+package com.greensnow25;
+
+import java.io.*;
+
+/**
+ * public class CheckByteStream check in that the recorded bit stream is an even number.
+ *
+ * @author grensnow25.
+ * @version 1.
+ * @since 13.02.17.
+ */
+public class CheckByteStream {
+    /**
+     * method check in that the recorded bit stream is an even number.
+     *
+     * @param in input stream.
+     * @return boolean value.
+     */
+    public boolean isNumber(InputStream in) {
+        boolean result = false;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            int line = Integer.parseInt(br.readLine());
+            if (line % 2 == 0) {
+                result = true;
+            } else {
+                System.out.println("This number is odd");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
