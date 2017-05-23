dc925ae88776762b53a789b21f318c977c3dd765
alexandr <greensnow25@yandex.ru>
2017-05-24 01:53:10 +0300

    Update date 24.05.17

--- chapter5/set/src/test/java/greensnow25/com/SimpleHashSet/SimpleHashSet.java
+++ chapter5/set/src/test/java/greensnow25/com/SimpleHashSet/SimpleHashSet.java
@@ -1,9 +1,6 @@
 package greensnow25.com.SimpleHashSet;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * public class SimpleHashSet.
@@ -12,27 +9,101 @@ import java.util.Set;
  * @version 1.
  * @since 23.05.2017.
  */
-public class SimpleHashSet<E> implements ISimpleHashSet<E>{
-
+public class SimpleHashSet<E> implements ISimpleHashSet<E> {
+    /**
+     * array of objects.
+     */
     private Object[][] set;
-    int io = 0;
+    /**
+     * size array.
+     */
+    private final int size;
 
-    public SimpleHashSet(int size){
-        this.set = new Object[size][size];
+    /**
+     * constructor.
+     *
+     * @param size size array.
+     */
+    public SimpleHashSet(int size) {
+        this.size = size;
+        this.set = new Object[this.size][this.size];
     }
 
-
+    /**
+     * add value to the set.
+     *
+     * @param e value.
+     * @return if ad original value successful return true, if add duplicate return false and.
+     * no addition to the array occurs.
+     */
     @Override
     public boolean add(E e) {
-        int h = e.hashCode();
+        int position = 0;
+        boolean canAdd = true;
+        int group = e.hashCode() % this.size;
+        for (int i = 0; i != set[group].length; i++) {
+            if (set[group][position] == null) break;
+            if (set[group][position].equals(e)) {
+                canAdd = false;
+                break;
+            }
+            position++;
+        }
+        if (canAdd) {
+            set[group][position] = e;
+            return true;
+        }
         return false;
     }
 
-
+    /**
+     * create Iterator object.
+     *
+     * @return iterator.
+     */
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            /**
+             * group is column of the array.
+             */
+            private int group = 0;
+            /**
+             * pointer is row of the array.
+             */
+            private int pointer = 0;
+
+            /**
+             * The method checks the presence of an element after the pointer.
+             * @return If after the pointer there are elements, return true, else false.
+             */
+            @Override
+            public boolean hasNext() {
+                for (; group != set.length; this.group++) {
+                    for (; pointer != set[group].length; this.pointer++) {
+                        if (set[group][pointer] != null) return true;
+                    }
+                    pointer = 0;
+                }
+                return false;
+            }
+
+            /**
+             * The method returns the next element after the pointer.
+             * @return element.
+             * @throws ArrayIndexOutOfBoundsException If a method is called .
+             * when there are no items after the pointer.
+
+             */
+            @Override
+            public E next() {
+                for (; ; this.group++) {
+                    for (; pointer != set[group].length - 1; this.pointer++) {
+                        if (set[group][pointer] != null) return (E) set[group][pointer++];
+                    }
+                    pointer = 0;
+                }
+            }
+        };
     }
-
-
 }
--- chapter5/set/src/test/java/greensnow25/com/SimpleHashSet/SimpleHashSetTest.java
+++ chapter5/set/src/test/java/greensnow25/com/SimpleHashSet/SimpleHashSetTest.java
@@ -1,5 +1,12 @@
 package greensnow25.com.SimpleHashSet;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -10,5 +17,90 @@ import static org.junit.Assert.*;
  * @since 23.05.2017.
  */
 public class SimpleHashSetTest {
+    /**
+     * class object.
+     */
+    private SimpleHashSet<String> set;
+
+    /**
+     * before the test.
+     */
+    @Before
+    public void beforeTheTest() {
+        this.set = new SimpleHashSet<>(16);
+        for (int i = 0; i != 3; i++) {
+            set.add("test" + i);
+        }
+    }
+
+    /**
+     * test add. If the value added is original,.
+     * then there is an addition to the set and the true is returned.
+     */
+    @Test
+    public void whenValueIsOriginThenAddToTheSet() {
+
+        assertTrue(set.add("test3"));
+    }
+
+    /**
+     * test add. If the value added is not original,.
+     * then there is no addition to the set and the false is returned.
+     */
+    @Test
+    public void whenValueIsNotOriginThenReturnFalse() {
+
+
+        assertFalse(set.add("test1"));
+    }
+
+    /**
+     * test iterator hasNext(). If after the pointer there are elements, return true.
+     */
+    @Test
+    public void whenCallHasNextThenReturnTrue() {
+
+        Iterator iterator = set.iterator();
+        iterator.next();
+        assertTrue(iterator.hasNext());
+    }
+
+    /**
+     * test iterator hasNext(). If after the pointer there are no elements, return false.
+     */
+    @Test
+    public void whenCallHasHextThenReturnFalse() {
+
+        Iterator iterator = set.iterator();
+        for (int i = 0; i != 3; i++) {
+            iterator.next();
+        }
+        assertFalse(iterator.hasNext());
+    }
+
+    /**
+     * test iterator next().If after the pointer there are elements, then return next element.
+     */
+
+    @Test
+    public void whenCallNextThenReturnElement() {
+
+        Iterator iterator = set.iterator();
+        iterator.next();
+
+        assertThat(iterator.next(), is("test0"));
+    }
+
+    /**
+     * test iterator next().If after the pointer there are no elements, then return Exception..
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenCallNextThenReturnException() {
+        Iterator iterator = set.iterator();
+        for (int i = 0; ; i++) {
+            iterator.next();
+        }
+    }
+
 
 }
