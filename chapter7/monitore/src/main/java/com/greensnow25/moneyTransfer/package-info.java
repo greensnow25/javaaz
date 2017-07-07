32c5be2043face9b9f5d2b5fadd22b987c51279f
greensnow <greensnow25@yandex.ru>
2017-07-07 16:32:55 +0300

    Update date 07.07.2017

---
+++ chapter7/monitore/src/main/java/com/greensnow25/moneyTransfer/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Public class package-info.
+ *
+ * @author greensnow25.
+ * @version 1.
+ * @since 07.07.2017.
+ */
+package com.greensnow25.moneyTransfer;
---
+++ chapter7/waitNotifyNotifyAll/src/main/java/com/greensnow25/PatternProducerCostumer.java
@@ -0,0 +1,11 @@
+package com.greensnow25;
+
+/**
+ * Public class PatternProducerCostumer.
+ *
+ * @author greensnow25.
+ * @version 1.
+ * @since 07.07.2017.
+ */
+public class PatternProducerCostumer {
+}
