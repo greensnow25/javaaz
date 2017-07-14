8213089b2bc42f2df432dbdc15b7472eb6146392
greensnow <greensnow25@yandex.ru>
2017-07-14 11:22:46 +0300

    Update date 14.07.2017

--- chapter7/waitNotifyNotifyAll/src/main/java/com/greensnow25/threadPool/SimpleThreadPool.java
+++ chapter7/waitNotifyNotifyAll/src/main/java/com/greensnow25/threadPool/SimpleThreadPool.java
@@ -1,8 +1,7 @@
 package com.greensnow25.threadPool;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Queue;
+import java.util.concurrent.*;
 
 /**
  * Public class SimpleThreadPool.
@@ -12,18 +11,85 @@ import java.util.concurrent.Executors;
  * @since 10.07.2017.
  */
 public class SimpleThreadPool {
+    /**
+     * Thread pool.
+     */
+    private final Thread[] pool;
+    /**
+     * Number of cores in the system.
+     */
+    private final int coreCount;
+    /**
+     * Task queue for execution.
+     */
+    private final ArrayBlockingQueue<Runnable> queue;
+    /**
+     * Thread position.
+     */
+    private int index = 0;
 
     /**
-     * method create ThreadPool equal to the number of processors in the system.
+     * Constructor.
+     */
+    public SimpleThreadPool() {
+        this.coreCount = Runtime.getRuntime().availableProcessors();
+        this.queue = new ArrayBlockingQueue(10);
+        this.pool = new Thread[coreCount];
+    }
+
+    /**
+     * Method create ThreadPool equal to the number of processors in the system.
      */
     public void makePool() {
-        int count = Runtime.getRuntime().availableProcessors();
-
-        ExecutorService service = Executors.newFixedThreadPool(count);
-        for (int i = 0; i != 100; i++) {
-            service.submit(new Work(i));
+        for (int i = 0; i != coreCount; i++) {
+            Thread q = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    synchronized (queue) {
+                        try {
+                            Thread.sleep(10);
+                            Runnable runnable = queue.poll(1, TimeUnit.SECONDS);
+                            if (runnable != null) {
+                                runnable.run();
+                            }
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+            });
+            pool[index++] = q;
+            q.start();
         }
-        service.shutdown();
+    }
+
+    /**
+     * destroy pool.
+     */
+    public synchronized void destroyPool() {
+        while (true) {
+            if (queue.isEmpty()) {
+                for (int i = 0; i != pool.length; i++) {
+                    System.out.println(pool[i].getName());
+                    pool[i].interrupt();
+                }
+                break;
+            }
+        }
+        System.out.println("Pool destroy.");
+    }
+
+    /**
+     * make a work.
+     */
+    public void makeAWork() {
+        for (int i = 0; i != 100; i++) {
+            try {
+                queue.put(new Work(i));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("All work is done.");
     }
 
     /**
@@ -34,6 +100,9 @@ public class SimpleThreadPool {
     public static void main(String[] args) {
         SimpleThreadPool stp = new SimpleThreadPool();
         stp.makePool();
+        stp.makeAWork();
+        stp.destroyPool();
+
     }
 
     /**
