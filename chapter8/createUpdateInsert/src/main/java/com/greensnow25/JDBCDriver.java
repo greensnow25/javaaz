b79255e3e66e438e5b43b0201469095dd4ee5418
greensnow <greensnow25@yandex.ru>
2017-08-03 08:17:26 +0300

    Update date 03.07.2017

--- chapter8/createUpdateInsert/src/main/java/com/greensnow25/JDBCDriver.java
+++ chapter8/createUpdateInsert/src/main/java/com/greensnow25/JDBCDriver.java
@@ -14,31 +14,44 @@ import java.util.ServiceLoader;
  * @since 02.08.2017.
  */
 public class JDBCDriver {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-    private static final String JDBC_URL = "jdbc:postgres:// 192.168.0.104/greensnow25";
-    URL result = this.getClass().getClassLoader().getResource("jdbc:postgres://127.0.0.1/greensnow25");
 
-    public void w() {
-        ServiceLoader<Driver> drivers = ServiceLoader.load(Driver.class);
-        for (Driver driver : drivers)
-            System.out.println(driver);
-    }
+        Connection connection = null;
+        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
+        String url = "jdbc:postgresql://127.0.0.1:5432/greensnow25";
+        //Имя пользователя БД
+        String name = "postgres";
+        //Пароль
+        String password = "tcrfylth";
+        try
 
-    public void q() {
-        try {
+        {
+            //Загружаем драйвер
             Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection(JDBC_URL, "postgres", "tcrfylth");
-            Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE tmp");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+            System.out.println("Драйвер подключен");
+            //Создаём соединение
 
-    public static void main(String[] args) {
-        JDBCDriver driver = new JDBCDriver();
-        driver.w();
+            connection = DriverManager.getConnection(url, name, password);
+            System.out.println("Соединение установлено");
+            //Для использования SQL запросов существуют 3 типа объектов:
+            //1.Statement: используется для простых случаев без параметров
+            Statement statement = null;
+
+            statement = connection.createStatement();
+            //Выполним запрос
+            ResultSet result1 = statement.executeQuery(
+                    "SELECT * FROM my_contacts where my_contacts.gender = 'M'");
+            //result это указатель на первую строку с выборки
+            //чтобы вывести данные мы будем использовать
+            //метод next() , с помощью которого переходим к следующему элементу
+            System.out.println("Выводим statement");
+            while (result1.next()) {
+                String nam = result1.getString("first_name");
+                System.out.println(nam);
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
