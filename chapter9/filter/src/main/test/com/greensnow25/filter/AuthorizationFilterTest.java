c1c93f83fc9ea6e5552a1170471334785b5759f2
greensnow <greensnow25@yandex.ru>
2017-09-27 15:24:54 +0300

    Update date 27.09.2017

--- chapter9/filter/src/main/test/com/greensnow25/filter/AuthorizationFilterTest.java
+++ chapter9/filter/src/main/test/com/greensnow25/filter/AuthorizationFilterTest.java
@@ -1,22 +1,27 @@
 package com.greensnow25.filter;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+
+//import javax.servlet.http.*;
+//import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 
@@ -48,6 +53,8 @@ public class AuthorizationFilterTest {
     private HttpServletResponse response;
     @Mock
     private FilterChain chain;
+    @Mock
+    private RequestDispatcher rd;
     @InjectMocks
     private AuthorizationFilter filter;
 
@@ -57,21 +64,38 @@ public class AuthorizationFilterTest {
         this.filter = new AuthorizationFilter();
     }
 
+    /**
+     * if the filter is successfully passed, pass on the chain.
+     *
+     * @throws IOException      ex.
+     * @throws ServletException ex.
+     */
     @Test
-    public void whenThen() throws IOException, ServletException {
+    public void whenFilterPassThenPassChain() throws IOException, ServletException {
         Cookie[] cookies = new Cookie[1];
         cookies[0] = new Cookie("user", "value");
         HttpSession session = mock(HttpSession.class);
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-        FilterChain fi = mock(FilterChain.class);
         when(request.getCookies()).thenReturn(cookies);
         when(request.getSession()).thenReturn(session);
         when(session.isNew()).thenReturn(true);
 
         doNothing().when(chain).doFilter(request, response);
-        filter.doFilter(req, resp, fi);
-        //filter.doFilter(request, response, chain);
+        filter.doFilter(request, response, chain);
         verify(chain, times(1)).doFilter(request, response);
+    }
+
+    /**
+     * if the filter is not successfully passed, pass on the login page.
+     *
+     * @throws IOException      ex.
+     * @throws ServletException ex.
+     */
+    @Test
+    public void whenPassThenPassLogin() throws IOException, ServletException {
+        when(request.getCookies()).thenReturn(null);
+        when(request.getRequestDispatcher(anyString())).thenReturn(rd);
+        doNothing().when(chain).doFilter(request, response);
+        filter.doFilter(request, response, chain);
+        verify(rd, times(1)).forward(request, response);
     }
 }
